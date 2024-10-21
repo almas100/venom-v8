fbca1c30a95d646593a797414d0a30a11ca5d1b8
almas100 <79959343+almas100@users.noreply.github.com>
2024-10-21 20:46:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,11 +38,11 @@ def install_requirements():
 install_requirements()
 
 # 🎛️ Telegram API token (replace with your actual token)
-TOKEN = '7424479555:AAGtNAbqXax_b3b--QnGYi5_XwBbQNw'
+TOKEN = '7621347499:AAH7TWosNrgwhP-HuetE5zWxGt4Lf_ft_oI'
 bot = telebot.TeleBot(TOKEN, threaded=False)
 
 # 🛡️ List of authorized user IDs (replace with actual IDs)
-AUTHORIZED_USERS = [5588464519]
+AUTHORIZED_USERS = [5756770855]
 
 # 🌐 Global dictionary to keep track of user attacks
 user_attacks = {}
